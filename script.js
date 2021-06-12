7179c11f202cf0a73b0d81d9660fab37d5f16749
Sam Nielsen <sammn721@gmail.com>
2021-06-11 18:58:53 -0700

    added my first bootstrap row and button

---
+++ script.js
@@ -0,0 +1,22 @@
+// for (var i = 0; i < array.length; i++) {
+//     const element = array[i];
+    
+// }
+
+var container = $('.container');
+
+
+
+
+container.append(`
+<form class="row row-cols-lg-auto g-3 align-items-center">
+  <div class="col-12">
+    <div class="input-group">
+      <div class="input-group-text">Time</div>
+      <input type="text" class="form-control" id="inlineFormInputGroupUsername" placeholder="Username">
+      <input class="btn btn-primary" type="button" value="Input">
+    </div>
+  </div>
+
+</form>
+`);
